ee0790009a05ee9a373f3a4c9365c03ad9472c72
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2025-05-25 15:42:45 +0330

    fade in

--- app/royal/components/hero.tsx
+++ app/royal/components/hero.tsx
@@ -1,53 +1,38 @@
-'use client'
-import React, { useRef } from 'react'
+ 
+import React from 'react'
 import { Bounded } from './Bounded'
 import Image from 'next/image'
 import heroImage from '../../../public/royal/hero-image.jpg'
 import Link from 'next/link'
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-
-gsap.registerPlugin(useGSAP);
+ 
+import { FadeIn } from './fade-in'
+ 
 
 const Hero = ( ) => {
   
-  const containerRef = useRef<HTMLDivElement>(null)
-  useGSAP(()=>{
-    gsap.to('.bg-image',{
-      scale:1,
-      opacity:0.5,
-      ease:'power3.out',
-      duration:5,
-    })
-  },{scope:containerRef})
+ 
   
   
   return (
     <Bounded
-    ref={containerRef}
+ 
         className={`relative w-full h-full  overflow-hidden bg-neutral-950 `}>
-            <figure className="bg-image opacity-0 absolute inset-0 scale-125 ">
+            <FadeIn vars={{ scale: 1, opacity: 0.5 }} className=" absolute inset-0 scale-125 ">
 
         <Image src={heroImage} priority alt='hero image' fill className='object-cover'/>
-            </figure>
+            </FadeIn>
         <div className="relative flex h-screen flex-col justify-center">
-        {/* <RevealText
-          field={slice.primary.heading}
-          id="hero-heading"
-          className="font-display max-w-xl text-6xl leading-none text-neutral-50 md:text-7xl lg:text-8xl"
-          staggerAmount={0.2}
-          duration={1.7}
-          as="h1"
-        /> */}
-        <h1  className="font-display max-w-xl text-6xl leading-none text-neutral-50 md:text-7xl lg:text-8xl">Effortless Elegance</h1>
+ 
 
-        {/* <FadeIn
-          className="mt-6 max-w-md translate-y-8 text-lg text-neutral-100"
+          <h1  className="font-display max-w-xl text-6xl leading-none text-neutral-50 md:text-7xl lg:text-8xl">Effortless Elegance</h1>
+        <FadeIn
+        // important factor to go up or down: translate-y-8
+          className="mt-6 max-w-md translate-y-8  text-lg text-neutral-100"
           vars={{ delay: 1, duration: 1.3 }}
         >
-          <PrismicRichText field={slice.primary.body} />
-        </FadeIn> */}
-        <p  className="mt-6 max-w-md translate-y-8 text-lg text-neutral-100" >An expression of quiet luxury, Côte Royale is designed for the man who commands attention without seeking it. A reflection of nature’s raw beauty.</p>
+        
+        <p  className=" " >An expression of quiet luxury, Côte Royale is designed for the man who commands attention without seeking it. A reflection of nature’s raw beauty.</p>
+        </FadeIn>
 
         {/* <FadeIn
           className="mt-8 translate-y-5"
@@ -62,8 +47,13 @@ const Hero = ( ) => {
             />
           ))}
         </FadeIn> */}
-        <Link  href={''} className="mt-12 w-fit inline-flex items-center justify-center px-12 py-4 text-center font-extrabold tracking-wider uppercase transition-colors duration-300  border border-white text-white hover:bg-white/20"
+         <FadeIn
+          className="mt-8 translate-y-5"
+          vars={{ delay: 1.7, duration: 1.1 }}
+        >
+        <Link  href={''} className=" w-fit inline-flex items-center justify-center px-12 py-4 text-center font-extrabold tracking-wider uppercase transition-colors duration-300  border border-white text-white hover:bg-white/20"
          >Shop now</Link>
+         </FadeIn>
       </div>
     </Bounded>
   )
